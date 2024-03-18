4867c9d4abbe76d7b3ec87473c4b27dbdb8424b9
Samuel Huwiler <samuel.huwiler@gmx.ch>
2024-03-18 14:24:44 +0100

    initialised python file

---
+++ vectorlib.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from typing import Sequence, Union
+
+
+class Vecotr:
+    """a python representation of a mathmatical Vector"""
+
+    def __init__(self, arg: Sequence[Union[int, float]]) -> None:
+        pass
+
+
+def main() -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
